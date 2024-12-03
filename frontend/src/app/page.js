81999aa8fdd99dfe256e61a4238f4717dfabaf3e
Rohit Waghode <rohitwaghode67@gmail.com>
2024-12-03 13:22:20 +0530

    baner add

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,12 @@
+import Baner from "@/components/Baner";
 import React from "react";
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Baner />
+    </div>
+  );
 };
 
 export default Page;
