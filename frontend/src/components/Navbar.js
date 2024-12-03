1b1e4fd2fdcdce899390f1ac4dc557e1abff133e
Rohit Waghode <rohitwaghode67@gmail.com>
2024-12-03 13:08:06 +0530

    navbar add

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b">
+      <div className="max-w-7xl mx-auto flex flex-items justify-between py-4">
+        <div>
+          <Link href={"/"}>
+            <div className="flex items-center gap-1">
+              <div className="relative">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/84/84145.png"
+                  width={30}
+                />
+              </div>
+              <span className="text-xl font-bold uppercase text-primary-500">
+                Yoga Products
+              </span>
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">
+            Sign in
+          </button>
+          <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
