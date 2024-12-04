8e6d03ce93cc411150fb5c15926f687e4caae27e
Rohit Waghode <rohitwaghode67@gmail.com>
2024-12-04 12:20:03 +0530

    file changes

---
+++ admin/src/app/add-products/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateProduct from ".";
+
+const AddProducts = () => {
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
+};
+
+export default AddProducts;
--- admin/src/components/Navbar.js
+++ admin/src/components/Navbar.js
@@ -29,9 +29,11 @@ const Navbar = () => {
           <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">
             Add Blogs
           </button>
-          <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">
-            Add Products
-          </button>
+          <Link href={"/add-products"}>
+            <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">
+              Add Products
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
