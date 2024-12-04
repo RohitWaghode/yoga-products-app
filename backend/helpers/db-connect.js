e7e40b5c1e840fd715421ca4bf432b84163cfbda
Rohit Waghode <rohitwaghode67@gmail.com>
2024-12-04 19:34:15 +0530

    atlas connect

--- backend/helpers/db-connect.js
+++ backend/helpers/db-connect.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const models = require("../models");
 
 module.exports = function initConnection() {
-  mongoose.connect("mongodb://localhost:27017/yoga-products-database");
+  mongoose.connect(
+    "mongodb+srv://rohitwaghode67:BfcJrK6x6BAW3T38@yoga-product-database.1ia13.mongodb.net/?retryWrites=true&w=majority&appName=yoga-product-database"
+  );
   let database = mongoose.connection;
   database.on("error", function (err) {
     console.log("Failed to connect database", err);
